3f4e02bad9f2b7b43c57bdcfc7642b7c8ef46287
Michael Skeen <mmskeen@gmail.com>
2020-03-24 17:23:12 -0600

    Uses method to get current year.

--- src/index.js
+++ src/index.js
@@ -6,3 +6,17 @@
 //E.g.
 //Created by Angela Yu.
 //Copyright 2019.
+
+import React from "react";
+import ReactDom from "react-dom";
+
+const name = "Michael Skeen";
+const year = new Date().getFullYear();
+
+ReactDom.render(
+  <div>
+    <p>Created by {name}</p>
+    <p>Copyright {year}</p>
+  </div>,
+  document.getElementById("root")
+);
